701ab759f965b3a3fe8d6dee8bcd5978d90af5dd
Ididksk <126318627+Ididksk@users.noreply.github.com>
2023-09-29 17:29:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id',
-POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'youre POSTGRESQL url',
+SESSION_ID: process.env.SESSION_ID || 'ZEROTWO=cjN3nbbK#g7RW0tHB_gv30MP54SNBzAQ_WWU9KDPlq5Bc4Km8dHE',
+POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://chariya_x5gi_user:owdt1CitQZSgcB67D5wsUlpSwuOaiijc@dpg-ck8s8tfsasqs738sajtg-a.frankfurt-postgres.render.com/chariya_x5gi',
 LANG: process.env.BOT_LANG || 'EN' ,
 ANTI_BAD: process.env.ANTI_BAD || 'false',
 MAX_SIZE: process.env.MAX_SIZE || 200,
